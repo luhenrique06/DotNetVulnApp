6e38f376f543faed63778cd55addc1f0c4937d5e
luhenrique06 <luhenrique06@gmail.com>
2023-07-06 23:45:24 -0300

    os injection

--- Controllers/UpdateDocsController.cs
+++ Controllers/UpdateDocsController.cs
@@ -12,11 +12,12 @@ namespace DotNetVulnApp.Controllers
     [Route("api/[controller]")]
     public class UpdateDocsController :ControllerBase
     {
-        [Authorize]
+
         [HttpPost]
+        [Authorize]
         public IActionResult PostNewDoc([FromBody] Docs doc){
             
-            string defaultPathForDocs = Directory.GetCurrentDirectory() + "/files";
+            string defaultPathForDocs = Directory.GetCurrentDirectory();
 
             if(doc.Content == null || doc.FileName == null)
                 return BadRequest("Missing mandatory parameters");
@@ -36,6 +37,46 @@ namespace DotNetVulnApp.Controllers
                 return BadRequest("Error while save doc");
             }
         }
+
+    [HttpGet]
+    [Route("download/{doc}")]
+    public async Task<IActionResult> getDoc(string doc){
+
+        string receiptBasePath = Directory.GetCurrentDirectory();
+
+        string receiptPath = doc + ".txt";
+
+        if(!System.IO.File.Exists(receiptPath))
+            return BadRequest("Doc não existe");
+
+        string papersize = Request.Query["size"];
+
+
+        string receiptPdfPath = "doc.pdf";
+        System.Diagnostics.ProcessStartInfo procStartInfo;
+        procStartInfo = new System.Diagnostics.ProcessStartInfo(
+            "/bin/bash", 
+            $"-c \"enscript {receiptPath} -o - | ps2pdf -dFIXEDMEDIA -sPAPERSIZE={papersize} - {receiptPdfPath}\""
+        );
+        
+
+        procStartInfo.UseShellExecute = false;
+        procStartInfo.CreateNoWindow = true;
+        procStartInfo.RedirectStandardError = true;
+        System.Diagnostics.Process proc = new System.Diagnostics.Process();
+        proc.StartInfo = procStartInfo;
+        proc.Start();
+        proc.WaitForExit();
+
+
+        string localPdfPath = System.IO.Path.Combine(
+            Directory.GetCurrentDirectory(),
+            receiptPdfPath
+        );
+
+        return PhysicalFile(localPdfPath, "application/pdf");
+    }
+
 
     }
 }
